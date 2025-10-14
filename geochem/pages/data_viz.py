42e678692849c18a5fb76e1ad6bec6b0980f0685
R. Caraballo <62677880+joiinar35@users.noreply.github.com>
2025-10-14 20:14:17 -0300

    Update data_viz.py

--- geochem/pages/data_viz.py
+++ geochem/pages/data_viz.py
@@ -174,10 +174,10 @@ def data_viz_callbacks(app):
     
         return fig
 
-# Callback to update the full correlation matrix heatmap
+    # Callback to update the full correlation matrix heatmap
     @app.callback(
         Output('correlation-matrix', 'figure'),
-        Input('tabs', 'value')  # Trigger when the Data Visualization tab is selected
+        Input('column-dropdown', 'value') # Trigger when the Data Visualization tab is selected
     )
     def update_full_correlation_matrix(tab_value):
         ''' Matrix de correlacion de todos los elementos '''
